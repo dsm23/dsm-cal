4754215da85283432113245b86366503373dc99c
David Murdoch <dsmurdoch357@gmail.com>
2020-07-16 00:10:13 +0100

    Update meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `David Murdoch cal example`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `This is a web native display inspired by the unix/linux cal terminal command`,
+    author: `dsm23`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
